806a503e5eb3be399f6f22b59ce35967abd334b1
Aleksa Piperski <mr.alexpiperski@gmail.com>
2025-06-20 17:10:56 +0200

    Added button logic for company inputs

--- src/components/Form/Input.js
+++ src/components/Form/Input.js
@@ -3,45 +3,57 @@ import { useState } from "react";
 const Input = (props) => {
   const [inputFields, setInputFields] = useState([
     { label: "Name", type: "text" },
-    { label: "E-mail", type: "email"},
-    { label: "Phone Number", type: "number"},
+    { label: "E-mail", type: "email" },
+    { label: "Phone Number", type: "number" },
+    { label: "Company Name", type: "text" },
   ]);
 
   const handleChange = (e) => {
     props.change(e);
   };
 
-  const handleAddButton = (e) => {};
+  const handleAddButton = () => {
+    let newCompany = { label: "Company Name", type: "text" };
+    setInputFields((prevInputs) => [...prevInputs, newCompany]);
+
+    console.log(inputFields);
+  };
+
+  let companyCounter = 0;
 
   const listItems = inputFields.map((labelInput, index) => {
-    const lowerCaseLabel = labelInput.label.toLocaleLowerCase();
+    let lowerCaseLabel = labelInput.label.toLocaleLowerCase();
+    let labelPlaceHolder = labelInput.label;
+    let buttonPlaceHolder = "";
+
+    if (labelInput.label === "Company Name") {
+      companyCounter++;
+      labelPlaceHolder = `${labelInput.label} ${companyCounter}`;
+      lowerCaseLabel = `${labelInput.label.toLocaleLowerCase()} ${companyCounter}`;
+    }
+
+    const isLastCompany =
+      labelInput.label === "Company Name" &&
+      index ===
+        inputFields.map((item) => item.label).lastIndexOf("Company Name");
+
+    if (isLastCompany) {
+      buttonPlaceHolder = <button onClick={handleAddButton}>+</button>;
+    }
+
     return (
       <div key={index}>
-        <label>{labelInput.label}</label>
+        <label>{labelPlaceHolder}</label>
         <input
           name={lowerCaseLabel}
           value={props.inputs[lowerCaseLabel] || ""}
           type={labelInput.type}
           onChange={handleChange}
         />
+        {buttonPlaceHolder}
       </div>
     );
   });
-  //   const inputArray = [
-  //     "Name",
-  //     "Email",
-  //     "Number",
-  //     "School Name",
-  //     "Title of study",
-  //     "Date of study",
-  //   ---------------------
-  //     "Company Name",
-  //     "Position title",
-  //     "Main responsibilities of your jobs",
-  //     "Start date",
-  //     "End date"
-  //   ---------------------
-  //   ];
   return <>{listItems}</>;
 };
 
