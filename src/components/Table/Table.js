6d273255d9198fc1f32b19cabddead951e568937
Aleksa Piperski <mr.alexpiperski@gmail.com>
2025-06-20 13:23:35 +0200

    Added reactive functionality to a TableRow component

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,8 +1,10 @@
+import TableRow from "./TableRow";
+
 const Table = (props) => {
   const handleEditClick = () => {
     props.editData(props.data);
   };
-  // make this component reactive and dyinamic foor each tr/td
+
   return (
     <>
       <table>
@@ -12,15 +14,7 @@ const Table = (props) => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Name: {props.data.name}</td>
-          </tr>
-          <tr>
-            <td>Email: {props.data.email}</td>
-          </tr>
-          <tr>
-            <td>Phone: {props.data.number}</td>
-          </tr>
+          <TableRow data={props.data} />
         </tbody>
       </table>
       <button onClick={handleEditClick}>Edit</button>
