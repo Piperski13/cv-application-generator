dfe2f0ed748f035ef50fa72dcd722144d1aaa8ac
Aleksa Piperski <mr.alexpiperski@gmail.com>
2025-07-04 17:18:05 +0200

    Added button components styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        merriweather: ["Merriweather", "serif"],
+        roboto: ["Roboto", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
