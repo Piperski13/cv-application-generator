a44458831f05813530be3161d91cba6f49b25d74
Aleksa Piperski <mr.alexpiperski@gmail.com>
2025-07-08 16:36:09 +0200

    Rendering responsive design

--- src/components/CompanyTable/CompanyTable.js
+++ src/components/CompanyTable/CompanyTable.js
@@ -7,14 +7,19 @@ const CompanyTable = (props) => {
   };
 
   return (
-    <div className="w-[750px] flex justify-between mb-6 pl-12">
+    <div className="flex justify-center flex-wrap mb-6 sm:pl-12 sm:w-[750px] sm:flex-nowrap sm:justify-between">
       <table>
         <thead>
           <tr>
             <th className="pt-7"></th>
           </tr>
-          <tr className="border-b-4 border-indigo-500">
+          <tr>
             <th className="text-left">Work experience</th>
+          </tr>
+          <tr>
+            <td colSpan="2">
+              <div className="h-1 bg-indigo-500 w-[350px] mx-auto my-2 sm:w-[575px]"></div>
+            </td>
           </tr>
         </thead>
         <tbody>
